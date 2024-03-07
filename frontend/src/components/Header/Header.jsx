44a1f2b2f3bb85a7e0aa9459b964bcd87dbe511f
victorvictoria-maker <victorvictoria0001@gmail.com>
2024-03-07 21:34:39 +0100

    User relogging after refreshing bug fixed

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -2,8 +2,10 @@ import logo from "../../assets/images/logo.png";
 import userImage from "../../assets/images/avatar-icon.png";
 import { BiMenu } from "react-icons/bi";
 
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useContext } from "react";
 import { NavLink, Link } from "react-router-dom";
+
+import { authContext } from "../../context/AuthContext";
 
 const navLinks = [
   {
@@ -27,6 +29,7 @@ const navLinks = [
 const Header = () => {
   const headerRef = useRef(null);
   const menuRef = useRef(null);
+  const { user, role, token } = useContext(authContext);
 
   const handleStickyHeader = () => {
     window.addEventListener("scroll", () => {
@@ -83,23 +86,31 @@ const Header = () => {
           </div>
 
           <div className='flex items-center gap-4'>
-            <div className='hidden'>
-              <Link to='/'>
-                <figure className='w-[35px] h-[35px] rounded-full cursor-pointer'>
-                  <img
-                    src={userImage}
-                    alt='User Image'
-                    className='w-full rounded-full'
-                  />
-                </figure>
+            {token && user ? (
+              <div>
+                <Link
+                  to={
+                    role === "doctor"
+                      ? "/doctors/profile/me"
+                      : "/users/profile/me"
+                  }
+                >
+                  <figure className='w-[35px] h-[35px] rounded-full cursor-pointer'>
+                    <img
+                      src={user?.photo}
+                      alt='Usersd Image'
+                      className='w-full rounded-full'
+                    />
+                  </figure>
+                </Link>
+              </div>
+            ) : (
+              <Link to='/login'>
+                <button className='bg-primaryColor py-2 px-6 text-white font-[600] h-[44px] flex items-center justify-center rounded-[50px]'>
+                  Login
+                </button>
               </Link>
-            </div>
-
-            <Link to='/login'>
-              <button className='bg-primaryColor py-2 px-6 text-white font-[600] h-[44px] flex items-center justify-center rounded-[50px]'>
-                Login
-              </button>
-            </Link>
+            )}
 
             <span className='md:hidden' onClick={toggleMenu}>
               <BiMenu className='w-6 h-6 cursor-pointer' />
