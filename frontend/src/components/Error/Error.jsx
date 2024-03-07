5e628f8bdd9eb7501d663e7bc2a5951c916e18db
victorvictoria-maker <victorvictoria0001@gmail.com>
2024-03-08 00:55:24 +0100

    User profile set up

---
+++ frontend/src/components/Error/Error.jsx
@@ -0,0 +1,13 @@
+/* eslint-disable react/prop-types */
+
+const Error = ({ errorMessage }) => {
+  return (
+    <div className='flex items-center justify-center w-full h-full'>
+      <h3 className='text-headingColor text-[20px] leading-[30px] font-semibold'>
+        {errorMessage}
+      </h3>
+    </div>
+  );
+};
+
+export default Error;
